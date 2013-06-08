f8590ede66b9f73e68afb4749e1a31636113dae9
MurilloArturo <amurillop90@gmail.com>
2013-06-07 20:20:03 -0430

    delete

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-HazTuPregunta::Application.config.secret_token = '43a68bedb35730b525bc051262d5e297a1b9d1a291ca5ac4891049304144bece922be041c41ec635bddd2d62987668817bec3f8f8941807193caeb0b27013ad0'
