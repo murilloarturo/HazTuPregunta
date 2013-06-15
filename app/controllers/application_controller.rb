5059bfdd3a12cb0cf85f395cf2d0059b8bea4ee9
MurilloArturo <amurillop90@gmail.com>
2013-06-15 12:12:11 -0430

    Login desde NavBar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
    	def authenticate_active_admin_user!
     	authenticate_user! 
     	unless current_user.superadmin?
-      		flash[:alert] = "Unauthorized Access!"
+      		flash[:alert] = "Acceso no autorizado!"
       		redirect_to root_path 
     	end
   	end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to @user, notice: 'Usuario creado exitosamente.' }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
@@ -65,7 +65,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'Datos de usuario actualizados.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
