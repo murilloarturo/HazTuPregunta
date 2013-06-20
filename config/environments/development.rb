2afcb39938e5fa0757c3f29d23f9bd923262bc9b
MurilloArturo <amurillop90@gmail.com>
2013-06-20 10:39:36 -0430

    email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Users::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -34,4 +34,17 @@ Users::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.perform_deliveries = true
+
+
+  #SMTP
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = {
+    :address  => "smtp.gmail.com",
+    :port     => 587,
+    :user_name  => '',
+    :password =>  '',
+  }
 end
